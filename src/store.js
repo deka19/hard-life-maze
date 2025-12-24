67319cf7b3f0b22f8871e340563feb36005537c1
deka19 <dekadhrubajyoti311@gmail.com>
2025-12-24 13:41:30 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { create } from 'zustand';
 
 export const useStore = create((set) => ({
-  gameState: 'MENU', // MENU, PLAYING, WON, DEAD
+  gameState: 'MENU', 
+  playerPos: [0,0,0], // New: Track position
+  setPlayerPos: (pos) => set({ playerPos: pos }),
+  
   startGame: () => set({ gameState: 'PLAYING' }),
-  endGame: (result) => set({ gameState: result }), // result = 'WON' or 'DEAD'
+  endGame: (result) => set({ gameState: result }),
   resetGame: () => set({ gameState: 'MENU' }),
 }));
