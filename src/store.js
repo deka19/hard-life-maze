5f4198708bc3e90ec3e1ce8379d985f3e64760ad
deka19 <dekadhrubajyoti311@gmail.com>
2025-12-24 13:50:23 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,10 +1,9 @@
 import { create } from 'zustand';
 
 export const useStore = create((set) => ({
-  gameState: 'MENU', 
-  playerPos: [0,0,0], // New: Track position
+  gameState: 'MENU', // This MUST be 'MENU' for the start button to show
+  playerPos: [0,0,0],
   setPlayerPos: (pos) => set({ playerPos: pos }),
-  
   startGame: () => set({ gameState: 'PLAYING' }),
   endGame: (result) => set({ gameState: result }),
   resetGame: () => set({ gameState: 'MENU' }),
